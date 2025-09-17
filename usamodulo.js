485e30202c34dbf7edbfb97af81e119dbff35a5e
vaguettibfd <vaguetti.bfd@gmail.com>
2025-09-17 06:41:07 -0300

    ajustes em usa módulo

--- usamodulo.js
+++ usamodulo.js
@@ -1,9 +1,9 @@
 // comando que importa as funcoes somar() e subtrair() para serem reutilizadas
 const calculadora = require('./biblioteca/definemodulo.js');
 var  x = 10, y = 30;
-console.log(calculadora.somar(x,y));
+console.log(calculadora.soma(x,y));
 console.log(calculadora.subtrair(y,x));
-console.log(calculadora.dividir(x,y));
-console.log(calculadora.multiplicar(x,y));
+//console.log(calculadora.dividir(x,y));
+//console.log(calculadora.multiplicar(x,y));
 
 //comentário de teste ...  novo teste
